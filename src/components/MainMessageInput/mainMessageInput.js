5658890ee1cc358066d43bb78b0de7d6637fed9e
Olya <okatranis@gmail.com>
2022-08-20 13:23:17 +0300

    add Last messages to contact

--- src/components/MainMessageInput/mainMessageInput.js
+++ src/components/MainMessageInput/mainMessageInput.js
@@ -1,10 +1,42 @@
-function MainMessageInput () {
-    return (
-        <form>
-            <input type="text" />
-            <button type="submit" onClick={() => props.addMessage}>Sent</button>
-        </form>
-    )
+import React from 'react';
+
+class MainMessageInput extends React.Component  {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ""
+        };
+        this.currentMessageChanged = this.currentMessageChanged.bind(this);
+    }
+
+    currentMessageChanged = (e) => {
+        this.setState({ message: e.target.value });
+    }
+
+    render () {
+
+        return (
+            <form action=''>
+                <input
+                    maxLength="500"
+                    type="text"
+                    value={this.state.message}
+                    placeholder="Type your message"
+                    onChange={this.currentMessageChanged}
+                />
+                <button
+                    type="submit"
+                    onClick={(e) => {
+                        e.preventDefault()
+                        this.props.addMessage(this.state.message)
+                    }}
+                >Send
+                </button>
+            </form>
+        )
+    }
+
 };
 
 export default MainMessageInput;
