ed26fa0963ce9e929fadbdf78d210680211311a4
Olga <okatranis@gmail.com>
2022-08-23 18:01:16 +0300

    Improved UI for message rows and search bar

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
 const SearchBar = (props) => {
-
-        return (
-            <form className="search-bar">
-                <i className="fa-solid fa-magnifying-glass icon"></i>
+    return (
+        <form className="search-bar">
+            <div className="search-bar-wrapper">
+                <i className="fa-solid fa-magnifying-glass icon"/>
                 <input
                     type="text"
                     placeholder="Search contact"
-                    className="search"
-                    onChange={(e) => {
-                        props.searchInputHandler(e.target.value)
-                    }
-                }
+                    className="search-input"
+                    onChange={(e) => {props.searchInputHandler(e.target.value)}}
                 />
-            </form>
-        )
+            </div>
+        </form>
+    )
 }
 
 export default SearchBar;
